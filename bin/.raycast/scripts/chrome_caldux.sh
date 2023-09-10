75d4baa955b7a330321061c4d47ef52805af58a2
Marco Caldera <marco.caldera93@icloud.com>
2023-09-10 14:39:25 +0200

    feat: add scripts

---
+++ bin/.raycast/scripts/chrome_caldux.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title chrome caldux
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName Script
+# @raycast.icon ../images/chrome.webp 
+
+open -na "Google Chrome" --args --profile-directory="Profile 2"
